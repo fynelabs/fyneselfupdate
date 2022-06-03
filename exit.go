c5bb0df533b2675742547fa5cbc22b2954ed61fa
Cedric BAIL <cedric@ddlm.me>
2022-06-03 14:57:31 -0600

    Add NewExitCallback

---
+++ exit.go
@@ -0,0 +1,33 @@
+package fyneselfupdate
+
+import (
+	"log"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/dialog"
+)
+
+// NewExitCallback properly exit a Fyne App. If a window is specified, it will Hide
+// the window first before exiting. It will also report error using dialog.ShowError
+// if the upgrade failed to Restart. If no window is specified, it will display error
+// on the command line.
+func NewExitCallback(app fyne.App, win fyne.Window) func(err error) {
+	if win != nil {
+		return func(err error) {
+			if err != nil {
+				win.Show()
+				dialog.ShowError(err, win)
+				return
+			}
+			win.Hide()
+			app.Quit()
+		}
+	}
+	return func(err error) {
+		if err != nil {
+			log.Printf("Unable to exit to a new version: %v\n", err)
+			return
+		}
+		app.Quit()
+	}
+}
