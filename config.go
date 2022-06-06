c2e17a60536772cf035a3976faf0c4de944c4729
Cedric BAIL <cedric@ddlm.me>
2022-06-06 14:56:39 -0600

    Add documentation.

--- config.go
+++ config.go
@@ -8,10 +8,14 @@ import (
 	"github.com/fynelabs/selfupdate"
 )
 
+// NewConfig create a selfupdate.Config structure that provide Fyne UI dialog to progress the update
 func NewConfig(app fyne.App, win fyne.Window, source selfupdate.Source, schedule selfupdate.Schedule, publicKey ed25519.PublicKey) *selfupdate.Config {
 	return NewConfigWithTimeout(app, win, 0, source, schedule, publicKey)
 }
 
+// NewConfigWithTimeout create a selfupdate.Config structure that provide Fyne UI dialog to progress the update
+// and provide a "default to yes" behavior if the user doesn't interact within timeout time to provide unattended
+// update.
 func NewConfigWithTimeout(app fyne.App, win fyne.Window, timeout time.Duration, source selfupdate.Source, schedule selfupdate.Schedule, publicKey ed25519.PublicKey) *selfupdate.Config {
 	return &selfupdate.Config{
 		Source:    source,
