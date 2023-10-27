bf53c9f40b61dc809c644504f5cf43ac3b8f9f81
Jacob <jacalz@tutanota.com>
2023-10-27 17:30:11 +0200

    Update the progress bar dialogs to not use deprecated apis

--- progress.go
+++ progress.go
@@ -2,39 +2,50 @@ package fyneselfupdate
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
 )
 
 // NewProgressCallback returns a callback that can be used to present download information during update.
 func NewProgressCallback(win fyne.Window) func(float64, error) {
 	var d dialog.Dialog
+	var progress fyne.Widget
 	return func(done float64, fail error) {
 		if d == nil {
 			if done < 0 {
-				//lint:ignore SA1019 upstream will provide a better solution
-				d = dialog.NewProgressInfinite("Application update", "Downloading update", win)
+				infinite := widget.NewProgressBarInfinite()
+				infinite.Start()
+				progress = infinite
 			} else {
-				//lint:ignore SA1019 upstream will provide a better solution
-				d = dialog.NewProgress("Application update", "Downloading update", win)
+				progress = widget.NewProgressBar()
 			}
+			content := container.NewVBox(widget.NewLabel("Downloading update"), progress)
+			d = dialog.NewCustomWithoutButtons("Application update", content, win)
 			d.Show()
 		}
 
-		if fail != nil {
+		cleanup := func() {
+			if infinite, ok := progress.(*widget.ProgressBarInfinite); ok {
+				infinite.Stop()
+			}
+
 			d.Hide()
 			d = nil
+		}
+
+		if fail != nil {
 			dialog.ShowError(fail, win)
+			cleanup()
 			return
 		}
 		if done == 1.0 {
-			d.Hide()
-			d = nil
+			cleanup()
 			return
 		}
 
-		//lint:ignore SA1019 here until upstream replaces
-		if progress, ok := d.(*dialog.ProgressDialog); ok {
-			progress.SetValue(done)
+		if limited, ok := progress.(*widget.ProgressBar); ok {
+			limited.SetValue(done)
 		}
 	}
 }
